ad62390d92a4c88b8604f72dd9c64050de7d9858
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-07 10:39:55 -0500

    Updated from Brython Server: 11/7/2017 10:39:54 AM

--- functionAnalysisProgram.py
+++ functionAnalysisProgram.py
@@ -1,5 +1,48 @@
-function = input('Choose highest power in function: ')
+function = input('Enter a function: ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+function = int(input('Choose highest power in function: '))
 while function > 0:
-    coefficient1 = input('Enter the coeffecient for the x^'+function+' term')
-    coefficient1 = input('Enter the coeffecient for the x^'+function-1+' term')
-    coefficient1 = input('Enter the coeffecient for the x^'+function+' term')
+    coefficient1 = int(input('Enter the coeffecient for the x^'+str(function)+' term: '))
+    data['coefficient'] = data['coefficient'] + str(coefficient1)
+    function = function-1
+if function > 0: 
+    coefficient1 = int(input('Enter the coeffecient for the x^'+str(function)+' term: '))
+
+if __name__ == '__main__':
+    data = {}
+    data['coefficient'] = ''
+"""
+    
