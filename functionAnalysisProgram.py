98cee88fd6af8d01fc8608814fea22d4887aca7a
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-08 10:01:11 -0500

    Updated from Brython Server: 11/8/2017 10:01:10 AM

--- functionAnalysisProgram.py
+++ functionAnalysisProgram.py
@@ -43,21 +43,26 @@ if function == 1:
     coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
     coefficient = int(input('Enter the constant: '))
 elif function == 2:
-    coefficient1 = int(input('Enter the coeffecient for the x^2 term: '))
-    coefficient2 = int(input('Enter the coeffecient for the x^1 term: '))
+    coefficient2 = int(input('Enter the coeffecient for the x^2 term: '))
+    coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
     coefficient = int(input('Enter the constant: '))
 elif function == 3:
-    coefficient1 = int(input('Enter the coeffecient for the x^3 term: '))
+    coefficient3 = int(input('Enter the coeffecient for the x^3 term: '))
     coefficient2 = int(input('Enter the coeffecient for the x^2 term: '))
-    coefficient3 = int(input('Enter the coeffecient for the x^1 term: '))
+    coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
     coefficient = int(input('Enter the constant: '))
 elif function == 4:
-    coefficient1 = int(input('Enter the coeffecient for the x^4 term: '))
-    coefficient2 = int(input('Enter the coeffecient for the x^3 term: '))
-    coefficient3 = int(input('Enter the coeffecient for the x^2 term: '))
-    coefficient4 = int(input('Enter the coeffecient for the x^1 term: '))
+    coefficient4 = int(input('Enter the coeffecient for the x^4 term: '))
+    coefficient3 = int(input('Enter the coeffecient for the x^3 term: '))
+    coefficient2 = int(input('Enter the coeffecient for the x^2 term: '))
+    coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
     coefficient = int(input('Enter the constant: '))
 elif function == 5:
-    
+    coefficient5 = int(input('Enter the coeffecient for the x^5 term: '))
+    coefficient4 = int(input('Enter the coeffecient for the x^4 term: '))
+    coefficient3 = int(input('Enter the coeffecient for the x^3 term: '))
+    coefficient2 = int(input('Enter the coeffecient for the x^2 term: '))
+    coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
+    coefficient = int(input('Enter the constant: '))
 
     
