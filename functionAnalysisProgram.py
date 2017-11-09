7c171577069339e3ebfb867088c7587b3135c11d
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-11-09 10:40:49 -0500

    Update functionAnalysisProgram.py

--- functionAnalysisProgram.py
+++ functionAnalysisProgram.py
@@ -6,6 +6,32 @@ for temp in func:
     
 """
 
+if __name__ == '__main__':
+    data = {}
+    data['coefficients'] = ''
+    data['derivative'] = ''
+
+
+function = int(input('Choose highest power in function: '))
+power = function
+while function >= 0:
+    coefficient = int(input('Enter the coefficient for the x^'+str(function)+' term: '))
+    data['coefficients'] = data['coefficients']+str(coefficient)
+    function = function-1
+
+list1 = data['coefficients'].split()
+for i in range(0,len(data['coefficients'])):
+    derivativecoeff = int(data['coefficients'][i])*power
+    while power > 1:
+        data['derivative'] = data['derivative']+str(derivative)
+    if power = 1:
+        data['derivative'] = data['derivative'] + 
+    
+    derivative = str(derivativecoeff)+'x^'+str(power-1)+'+'
+    power+=-1
+    data['derivative'] = data['derivative']+str(derivative)
+    
+print(data['derivative'])
 
 
 
@@ -34,10 +60,7 @@ for temp in func:
 
 
 
-
-
-
-
+"""
 function = int(input('Choose highest power in function: '))
 if function == 1:
     coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
@@ -64,5 +87,5 @@ elif function == 5:
     coefficient2 = int(input('Enter the coeffecient for the x^2 term: '))
     coefficient1 = int(input('Enter the coeffecient for the x^1 term: '))
     coefficient = int(input('Enter the constant: '))
-
+"""
     
